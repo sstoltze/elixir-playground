e26c5d87b33e7fdda52745134a57306a7ad1b3cb
Sarah Ella Stoltze <sstoltze@gmail.com>
2023-11-22 16:53:11 +0100

    Move things around a bit

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A template that shows all standard flake outputs";
+  description = "Elixir/phoenix setup";
 
   # Inputs
   # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
